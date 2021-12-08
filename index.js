e62d53682219bc38b4c6ac8798cd41bcb7b19d50
becca <rebecca.kaci@gmail.com>
2021-12-08 16:47:50 +0100

    ajout d'une nouvelle langue

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const engine = require('express-handlebars').engine
+const translations = require('./translation.json')
+
+app.engine("handlebars", engine())
+app.set('view engine', 'handlebars')
+
+// je rends le dossier public accessible
+app.use(express.static('public'))
+
+app.get('/:lang?', (req, res) => {
+    console.log(req.params) // pour récupérer les paramètres dynamiques de l'url
+
+    let { lang } = req.params
+
+    if (!translations[lang]) {
+        lang = "fr"
+    }
+
+    res.render('home', {
+        pageTitle: translations[lang].pageTitle,
+        title: translations[lang].title,
+        src: `/images/${lang}.jpeg`
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+  })
